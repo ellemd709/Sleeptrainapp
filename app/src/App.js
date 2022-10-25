1234f3617b71e655486eaf82316244f955973721
Lisa Doody <101128408+ellemd709@users.noreply.github.com>
2022-10-25 12:04:45 -0230

    register

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Test from './components/test';
 import Login from './components/login';
+import Register from './components/Register';
+
 function App() {
   return (
     <div className="App">
    <Login/>
+   <Register/>
     </div>
   );
 }
